f3a94f59ddd0ec3eff50e65a3fb196308de92541
Stegin <steginmrajeev@gmail>
2023-03-27 21:39:49 +0530

    todo app

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,10 +1,3 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (request, response) => {
-  response.send("hello world");
-});
-
 app.listen(3000, () => {
   console.log("started express server at port 3000");
 });
