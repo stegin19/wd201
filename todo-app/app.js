a86e62931c05e2b5b4ce63de9947b3391f14e4fc
Stegin <steginmrajeev@gmail>
2023-04-01 23:20:11 +0530

    9

--- todo-app/app.js
+++ todo-app/app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
 app.get("/", async (request, response) => {
-  const allTodos = await Todo.retriveTodos();
+  const allTodos = await Todo.getTodos();
   if (request.accepts("html")) {
     response.render("index.ejs", {
       allTodos,
